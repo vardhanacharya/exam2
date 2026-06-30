bcfaa210a5138df201b9b363bf2c4e55553984a0
vardhanacharya <collegevardhan@gmail.com>
2026-06-30 15:29:15 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{textAlign:'center',marginTop:'100px'}}>
+      <h1>React CI/CD Project</h1>
+      <h2>Hello Docker Swarm!</h2>
+    </div>
+  );
+}
+
+export default App;
